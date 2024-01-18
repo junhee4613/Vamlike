3769e3f2d454f22c16ccfd12d91d57deb276b05d
junhee4613 <junhee000123@naver.com>
2024-01-18 16:02:14 +0900

    특강 3주차(베지어 곡선, 쉴드 쉐이더)

---
+++ vam/Assets/02.Scripts/Shooter.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] public GameObject missile;
+    [SerializeField] public GameObject target;
+    [SerializeField] public float spd;
+    [SerializeField] public int shot = 12;
+
+    public void Shot()
+    {
+        StartCoroutine(CreateMissile());
+    }
+
+    IEnumerator CreateMissile()
+    {
+        int _shot = shot;
+        while (_shot > 0)
+        {
+            _shot--;
+            GameObject temp = Instantiate(missile, transform.position, Quaternion.identity);
+            temp.GetComponent<BezieMissile>().master = gameObject;
+            temp.GetComponent<BezieMissile>().enemy = target;
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return null;
+    }
+}
